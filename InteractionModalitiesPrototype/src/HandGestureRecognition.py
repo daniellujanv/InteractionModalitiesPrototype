67118c3052938d94101d82e58a26bf7b1dc47206
Daniel Lujan <dluj@itu.dk>
2014-05-12 22:34:30 +0200

    - bgrTOhsv - biggestContour - converhull - convexity defects - video processing

--- InteractionModalitiesPrototype/src/HandGestureRecognition.py
+++ InteractionModalitiesPrototype/src/HandGestureRecognition.py
@@ -1,6 +1,6 @@
 import cv2
-import Tools as tools
 from Tools import RegionProps
+import Tools as tools
 
 global regionProps
 regionProps = RegionProps()
@@ -11,29 +11,40 @@ regionProps = RegionProps()
 - restrict channels
 - biggest contour as hand
 '''
-def approach2(img):
+def handRecognition(img, hmax, hmin, smax, smin):
     imgColor = img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     
-    minRange = cv2.cv.Scalar(1.0 , 94.0, 1.0)
-    maxRange = cv2.cv.Scalar(29.0, 255.0, 255.0)
+    minRange = cv2.cv.Scalar(hmin , smin, 1.0)
+    maxRange = cv2.cv.Scalar(hmax, smax, 255.0)
     img = cv2.inRange(img, minRange, maxRange)
+    img = 255 - img
+    cv2.imshow("Details", img)
     #tools.showImages(testing=img)
-    contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     #biggestContour[0] == contour, biggestContour[1] == props
-    biggestContour = contours[0]
-    biggestContourProps = regionProps.CalcContourProperties(contours[0], ["Area", "Boundingbox", "Centroid", "Extend"])
-    for contour in contours:
-        #print contour
-        props = tools.RegionProps.CalcContourProperties(contour, ["Area", "Boundingbox", "Centroid", "Extend"])
-        if(biggestContourProps["Area"] < props["Area"]):        
-            biggestContour = contour
-            biggestContourProps = props
-
-    center = (int(biggestContourProps["Centroid"][0]), int(biggestContourProps["Centroid"][1]))
-    cv2.circle(imgColor, center, 5, (0, 0, 255), (int(biggestContourProps["Area"]*.00005) + 1) ) 
-    cv2.drawContours(imgColor, [biggestContour], -1, (0, 255, 0))
-    hull =  cv2.convexHull(biggestContour)
-    cv2.drawContours(imgColor, [hull], -1, (0, 0, 255))
+    if(len(contours) > 0):
+        biggestContour = contours[0]
+        biggestContourProps = regionProps.CalcContourProperties(contours[0], ["Area", "Centroid"])
+        for contour in contours:
+            #print contour
+            props = regionProps.CalcContourProperties(contour, ["Area", "Centroid"])
+            if(biggestContourProps["Area"] < props["Area"]):        
+                biggestContour = contour
+                biggestContourProps = props
+    
+        center = (int(biggestContourProps["Centroid"][0]), int(biggestContourProps["Centroid"][1]))
+        cv2.circle(imgColor, center, 5, (0, 0, 255), (int(biggestContourProps["Area"]*.00005) + 1) ) 
+        cv2.drawContours(imgColor, [biggestContour], -1, (0, 255, 0))
+        convexhull = cv2.convexHull(biggestContour, returnPoints = False)
+        defects = cv2.convexityDefects(biggestContour, convexhull)
+        for defect in defects:
+            start_i, end_i, far_i, _ = defect[0]
+            start = tuple(biggestContour[start_i][0])
+            end = tuple(biggestContour[end_i][0])
+            far = tuple(biggestContour[far_i][0])
+            cv2.line(imgColor, start, end, [0,255,0], 2)
+            cv2.circle(imgColor, far, 5, [0,0,255], -1)
+        #cv2.drawContours(imgColor, [convexhull], -1, (0, 0, 255))
     
     return imgColor
--- InteractionModalitiesPrototype/src/InteractionModalities.py
+++ InteractionModalitiesPrototype/src/InteractionModalities.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import HandGestureRecognition
+import HandGestureRecognition as HGRecon
 
 
 def getImageSequence(capture, fastForward):
@@ -12,54 +12,104 @@ def getImageSequence(capture, fastForward):
         frameNumber = frameNumber+1
     return originalImage, isSequenceOK
 
-def run(speed,video): 
+def printUsage():
+    print "Q or ESC: Stop"
+    print "SPACE: Pause"
     
+def run(speed,video): 
+    global image, repeat
     '''MAIN Method to load the image sequence and handle user inputs'''   
-
     #--------------------------------video
+    setupWindowSliders()
     capture = cv2.VideoCapture(video)
     tempSpeed = 1
+    while repeat:
+        image, isSequenceOK = getImageSequence(capture,speed)
+        image = cv2.resize(image, size1)
+        #print isSequenceOK
+        #H,W,_ = image.shape
+        #record = False
+        if(isSequenceOK):
+            processImage(image)
+            printUsage()
+        #writer = cv2.VideoWriter('CubeProjections.avi', cv.CV_FOURCC('D','I','V','3'), 5.0, (W,H), True)
+        while(isSequenceOK):
+            #OriginalImage = image.copy()
+            image = cv2.resize(image, size1)
+            inputKey = cv2.waitKey(1)
+            if inputKey == 32:#  stop by SPACE key
+                sliderVals = getSliderVals()
+                cv2.setTrackbarPos('Stop/Start','Threshold',not sliderVals['Running'])
+                #update(OriginalImage)
+                if speed==0:     
+                    speed = tempSpeed;
+                else:
+                    tempSpeed=speed
+                    speed = 0;                    
+            if (inputKey == 27) or (inputKey == ord('q')):#  break by ECS key
+                repeat=False
+                break    
+            #get next sequence and update
+            if (speed>0):
+                processImage(image)
+                image, isSequenceOK = getImageSequence(capture,speed)
 
-    image, isSequenceOK = getImageSequence(capture,speed)
-    print isSequenceOK
-    H,W,_ = image.shape
-    record = False
 
-    if(isSequenceOK):
-        update(image)
-        #printUsage()
-    
-    #writer = cv2.VideoWriter('CubeProjections.avi', cv.CV_FOURCC('D','I','V','3'), 5.0, (W,H), True)
-    while(isSequenceOK):
-        OriginalImage = image.copy()
-     
-        
-        inputKey = cv2.waitKey(1)
-        
-        if inputKey == 32:#  stop by SPACE key
-            update(OriginalImage)
-            if speed==0:     
-                speed = tempSpeed;
-            else:
-                tempSpeed=speed
-                speed = 0;                    
-            
-        if (inputKey == 27) or (inputKey == ord('q')):#  break by ECS key
-            break    
-                
-        if inputKey == ord('p') or inputKey == ord('P'):
-            global ProcessFrame
-            if ProcessFrame:     
-                ProcessFrame = False;
-                
-            else:
-                ProcessFrame = True;
-            update(OriginalImage)
 
-def update(image):
-    
-    cv2.imshow("Result", image)
+def processImage(image):
+    sliderVals = getSliderVals()
+    processed = HGRecon.handRecognition(image, sliderVals["Hmax"], sliderVals["Hmin"], sliderVals["Smax"], sliderVals["Smin"])
+
+    cv2.imshow("Result", processed)
+    #cv2.imshow("Details", image)
+
+def setupWindowSliders():
+    ''' Define windows for displaying the results and create trackbars'''
+    cv2.namedWindow("Result")
+    cv2.namedWindow('Threshold')
+    cv2.namedWindow("Details")
+    cv2.resizeWindow('Threshold', 400, 30)
+    #Threshold value for the pupil intensity
+    #Hue 0 - 179
+    #Sat 0 - 255
+    cv2.createTrackbar('Hue min','Threshold', 1, 179, onSlidersChange)
+    cv2.createTrackbar('Sat min','Threshold', 1, 255, onSlidersChange)
+    cv2.createTrackbar('Hue max','Threshold', 120, 179, onSlidersChange)
+    cv2.createTrackbar('Sat max','Threshold', 50, 255, onSlidersChange)
+    #Value to indicate whether to run or pause the video
+    cv2.createTrackbar('Stop/Start','Threshold', 0,1, onSlidersChange)
+
+def getSliderVals():
+    '''Extract the values of the sliders and return these in a dictionary'''
+    sliderVals={}
+    sliderVals['Hmax'] = cv2.getTrackbarPos('Hue max', 'Threshold')
+    sliderVals['Hmin'] = cv2.getTrackbarPos('Hue min', 'Threshold')
+    sliderVals['Smax'] = cv2.getTrackbarPos('Sat max', 'Threshold')
+    sliderVals['Smin'] = cv2.getTrackbarPos('Sat min', 'Threshold')
+    sliderVals['Running'] = 1==cv2.getTrackbarPos('Stop/Start', 'Threshold')
+    return sliderVals
+
+def onSlidersChange(dummy=None):
+    ''' Handle updates when slides have changed.
+     This  function only updates the display when the video is put on pause'''
+    global image
+    sv=getSliderVals()
+    if(not sv['Running']): # if pause
+        global speed
+        speed = 0
+        processImage(image)
 
 #---------------------------------- main method -------------------------------------------------#
-video = 'videos/startStop'
-run(1, video)
+global frameNumber
+global image
+global size1
+global speed
+global repeat
+
+size1 = (450,350)
+frameNumber = 0
+speed = 1
+repeat = True
+
+video = 'videos/testHandRecon.mov'
+run(speed, video)
